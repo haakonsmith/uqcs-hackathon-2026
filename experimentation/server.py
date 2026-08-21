5fdb3515d65857ed58c0c75f7ebb93a679aa2dd0
mason-stonemason <mason.steinmetz06@gmail.com>
2026-08-22 07:27:58 +1000

    Forget it classes are better instead of dictionaries as we are not actually sending any user data from server to client

--- experimentation/server.py
+++ experimentation/server.py
@@ -4,14 +4,48 @@ import time
 import random
 
 from websockets.asyncio.server import serve
+from dataclasses import dataclass
+
 gameState = {};
 player = {};
 world = {};
 territory = {};
 
+def createWorld():
+    return {
+        "numTerritories": 0
+    }
+
+
+@dataclass
+class currentAction:
+    mouseHover: bool = False
+    mousePos: tuple = (0, 0)
+    mouseTile: tuple = (0, 0)
+    mouseClick: bool = False #temp i guess
+
+
+class player:
+    def __init__(self, userID, name, status, territory):
+        self.userID = userID;
+        self.name = name;
+        self.status = status;
+        self.territory = territory;
+        self.points = 0;
+
+        self.connectionTime = time.time(),
+        self.lastSeen = self.connectionTime,
+        self.currentAction = currentAction()
+
+class territory:
+    def __init__(self, name, id):
+        self.name = name;
+        self.id = id;
+
 def createPlayer(websocket):
     playerId = str(websocket.id); #uuid??
-
+    #player = player(playerId, name, status, territory)
+    """
     return {
         "userID": playerId, #idk what player values to add except userID as primary
         "connectionTime": time.time(),
@@ -27,14 +61,19 @@ def createPlayer(websocket):
             "mouseTile": (0,0)
         }
     }
+    """
+
 
 def createTerritory(name, id): #no arguments for now 
     return {
         "name": name,
         "id": id,
-        "colour": random.randint(1, 15), #maybe int represents colour, like 1=blue
+        "colour": random.randint(1, 15), #maybe int represents colour, like 1=blue (random for now)
     }
 
+async def initiate(websocket):
+    player[websocket] = createPlayer(websocket);
+    world[websocket] = createWorld();
 
 async def game(websocket):
     player[websocket] = createPlayer(websocket);
