87014ad3d24e2e40ccaad39d1e0d3bb35eca3555
mason-stonemason <mason.steinmetz06@gmail.com>
2026-08-22 08:27:47 +1000

    Made classes for player, fixed up client handling

--- experimentation/server.py
+++ experimentation/server.py
@@ -5,9 +5,11 @@ import random
 
 from websockets.asyncio.server import serve
 from dataclasses import dataclass
+import websockets
+import player
 
 gameState = {};
-player = {};
+players = {};
 world = {};
 territory = {};
 
@@ -17,52 +19,10 @@ def createWorld():
     }
 
 
-@dataclass
-class currentAction:
-    mouseHover: bool = False
-    mousePos: tuple = (0, 0)
-    mouseTile: tuple = (0, 0)
-    mouseClick: bool = False #temp i guess
-
-
-class player:
-    def __init__(self, userID, name, status, territory):
-        self.userID = userID;
-        self.name = name;
-        self.status = status;
-        self.territory = territory;
-        self.points = 0;
-
-        self.connectionTime = time.time(),
-        self.lastSeen = self.connectionTime,
-        self.currentAction = currentAction()
-
 class territory:
     def __init__(self, name, id):
         self.name = name;
         self.id = id;
-
-def createPlayer(websocket):
-    playerId = str(websocket.id); #uuid??
-    #player = player(playerId, name, status, territory)
-    """
-    return {
-        "userID": playerId, #idk what player values to add except userID as primary
-        "connectionTime": time.time(),
-        "lastSeen": time.time(), #for client heartbeat
-        "name": None, #no name yet (idk if we will ask names)
-        "status": "Unknown",
-
-        "territory": None,
-        "points": 0,
-        "currentAction": {
-            "mouseHover": False,
-            "mousePos": (0,0),
-            "mouseTile": (0,0)
-        }
-    }
-    """
-
 
 def createTerritory(name, id): #no arguments for now 
     return {
@@ -71,23 +31,29 @@ def createTerritory(name, id): #no arguments for now
         "colour": random.randint(1, 15), #maybe int represents colour, like 1=blue (random for now)
     }
 
-async def initiate(websocket):
-    player[websocket] = createPlayer(websocket);
-    world[websocket] = createWorld();
 
-async def game(websocket):
-    player[websocket] = createPlayer(websocket);
+async def startMenu(websocket):
+    content = await websocket.recv();
+    if (content == "joinGame"):
+        players[websocket] = player(websocket);
+        await websocket.send(f"Logging in");
+    else:
+        await websocket.send("invalid initation (temp)");
 
-    #name = await websocket.recv();
-    #print(f"<<< {name}")
+async def clients(websocket):
+    #players[websocket] = player(websocket);
 
-    #greeting = f"Hello {name}!"
+    try:
+        await startMenu(websocket);
 
-    #await websocket.send(greeting)
-    #print(f">>> {greeting}")
+    except: websockets.exceptions.ConnectionClosed;
+
+    finally:
+        if websocket in players:
+            del players[websocket];
 
 async def main():
-    server = await serve(game, "localhost", 8765)
+    server = await serve(clients, "localhost", 8765)
     await server.serve_forever()
 
 if __name__ == "__main__":
